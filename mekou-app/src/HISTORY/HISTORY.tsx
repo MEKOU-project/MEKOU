75d2bdc9be63971781a7c9989e8b8effea422f94
amyoshi <narinarit59@gmail.com>
2026-02-16 00:01:21 +0900

    build succes

--- mekou-app/src/HISTORY/HISTORY.tsx
+++ mekou-app/src/HISTORY/HISTORY.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 // 実際には fetch や import historyData from './history.json' で読み込む
